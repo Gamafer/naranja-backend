332678a97fff0578fdda7b3a496dcd287a105ac6
Fernando Garrote De La Macorra <42215154+FernandoGarrote@users.noreply.github.com>
2023-07-20 23:06:43 +0200

    Conexión a Render

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require ('express')
 const cors = require ('cors');
 const jwt = require ("jsonwebtoken");
@@ -6,25 +8,31 @@ const {readdirSync} = require('fs')
 const app = express()
 const jwtSecret = process.env.JWT_SECRET;
 
-require('dotenv').config()
-
 const PORT = process.env.PORT
 
 //Router principal
 const generalRouter = require('./routes');
  //middlewares   
-app.use(express.json())
-app.use(cors())
 app.use(generalRouter);
 
 //routes
  readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
- //.get inicial para testeo
- app.get("/", (req, res) => {
-    res.send({msg: "Hola"});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", (req, res) => {
+    console.log("Petición a TEST");
+    res.json({
+        success:true, 
+        message: "¡API Respondiendo!",
+    });
 });
 
+app.listen(process.env.PORT || '0.0.0.0:$PORT', () => {
+    console.log("Servidor rulando");
+})
 
 
 const server = () => {
@@ -34,4 +42,3 @@ const server = () => {
     })
 }
 
-server()
